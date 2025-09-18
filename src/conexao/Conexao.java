8e7b07d9034bd0de0f730aa0778d0c54034f432e
Gustavo <ghgennari@gmail.com>
2025-09-18 11:02:22 -0300

    Create Conexao.java

---
+++ src/conexao/Conexao.java
@@ -0,0 +1,24 @@
+package conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class Conexao {
+    public Connection getConexao(){
+        try{
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/bdatv01?useTimezone=true&serverTimezone=UTC",
+                    "root","");
+            System.out.println("Conexão realiada");
+            return conn;
+        }
+        catch(Exception e){
+            System.out.println("Erro ao conectar"+e.getMessage());
+            return null;
+        }
+    }
+}
