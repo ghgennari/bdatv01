5ffd0405f93b2407ec9a643b045bc021892df4e5
Nicolas Augusto <nick.nogueira02@gmail.com>
2025-09-18 10:45:09 -0300

    Update Conexao.java

--- src/conexao/Conexao.java
+++ src/conexao/Conexao.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package conexao;
 
-/**
- *
- * @author ghgen
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Conexao {
-    
+     public Connection getConexao(){
+        try{
+            Connection conn = DriverManager.getConnection("jbdc:mysql://localhost:3306/bdatv01?useTimezone=true&serverTimezone=UTC",
+                    "root", "");
+            System.out.println("Conexão realizada com sucesso!");
+            return conn;
+        }
+        catch (Exception e){
+            System.out.println("Erro ao conectar no BD" + e.getMessage());
+            return null;
+        }
+    }
+
 }
