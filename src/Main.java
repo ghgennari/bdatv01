c59daa8c10f5d2b47e391606779f6e2ce12188d6
Gustavo <ghgennari@gmail.com>
2025-09-18 10:57:38 -0300

    Conexao, Model, DAO

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+import conexao.Conexao;
+
+/**
+ *
+ * @author ghgen
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Conexao c = new Conexao();
+        c.getConexao();
+    }
+    
+}
