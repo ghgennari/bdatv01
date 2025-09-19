11d7aac3b75a6c772667fd18cb6023633d6441a8
Gustavo <ghgennari@gmail.com>
2025-09-19 20:10:36 -0300

    Update Cliente.java

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
 /**
@@ -10,5 +5,51 @@ package model;
  * @author ghgen
  */
 public class Cliente {
+    private int id_cliente;
+    private String nome;
+    private String endereco;
+    private String email;
+    private String telefone;
+
+    public int getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(int id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
     
 }
